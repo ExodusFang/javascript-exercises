4305fea85a30f7332647654301dc24803b70f156
ExodusFang <exodusfang@gmail.com>
2026-05-23 18:40:58 +1000

    Add conversion functionality to and from celsius to stubs

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundToOneDecimal = function(num) {
+  return Math.round(num*10)/10;
+}
+
+const convertToCelsius = function(fTemp) {
+  return roundToOneDecimal((fTemp - 32) * 5/9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  return roundToOneDecimal(cTemp*9/5+32);
 };
 
 // Do not edit below this line
