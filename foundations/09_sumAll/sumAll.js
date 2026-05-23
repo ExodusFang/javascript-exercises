3108c0d386aa9b8eedb5e9b96b69a7d4a89a1376
ExodusFang <exodusfang@gmail.com>
2026-05-23 18:12:04 +1000

    Add summing function to stub

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let trueStart = start;
+    let sum = 0;
 
+    if (start > end) {
+        trueStart = end;
+        end = start;
+    }
+
+    for (let i = trueStart; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
