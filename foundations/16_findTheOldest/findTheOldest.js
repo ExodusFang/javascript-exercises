7724560e94905ed269faa0bf337803bfe666b808
ExodusFang <exodusfang@gmail.com>
2026-05-24 19:24:11 +1000

    Unskip tests, add check for yearOfDeath key, confirm functionality

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
 const findTheOldest = function(personArr) {
-    const calculateAge = person => person.yearOfDeath - person.yearOfBirth;
+    const CURRENT_YEAR = new Date().getFullYear();
+    const calculateAge = person => "yearOfDeath" in person ? person.yearOfDeath - person.yearOfBirth : CURRENT_YEAR - person.yearOfBirth;
     return personArr.reduce((prev, curr) => calculateAge(curr) > calculateAge(prev) ? curr : prev);
 };
 
