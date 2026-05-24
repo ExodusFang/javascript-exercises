778a77437c01d2c743f80041d7687719a26a1b66
ExodusFang <exodusfang@gmail.com>
2026-05-24 19:00:25 +1000

    Complete fibonacci() stub

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let prev = 0;
+    let curr = 1;
+    let placeholder;
 
+    num = +num;
+
+    for (let i = 1; i < num; i++) {
+        placeholder = curr;
+        curr += prev;
+        prev = placeholder;
+    }
+
+    if (num === 0) curr = prev;
+    else if (num < 0) return "OOPS";
+
+    return curr;
 };
 
 // Do not edit below this line
