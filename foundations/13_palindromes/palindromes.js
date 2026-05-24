d9af443395c5fca2f5df4dbd3097139c34c36184
ExodusFang <exodusfang@gmail.com>
2026-05-24 18:52:16 +1000

    Amend code to match solution (more efficient)

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function (str) {
     const strArr = str.toLowerCase().match(/[a-z0-9]/g);
-    return strArr.every((item, index) => item === strArr.at(-(index + 1)));
+    return strArr.join("") === strArr.reverse().join("");
 };
 
 // Do not edit below this line
