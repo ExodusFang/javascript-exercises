ff0d3cedea3f51f91e74f7115680272142ce9e90
ExodusFang <exodusfang@gmail.com>
2026-05-24 18:40:21 +1000

    Complete palindromes() stub

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    return !str.split("")
+               .some(
+                    (item, index) => strArr[index] != strArr[-(index + 1)]
+                );
 };
 
 // Do not edit below this line
