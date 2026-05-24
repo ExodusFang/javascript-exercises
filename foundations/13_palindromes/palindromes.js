2248f65b1425113ac5906f11c386b771e67bede2
ExodusFang <exodusfang@gmail.com>
2026-05-24 18:41:50 +1000

    Fix missing var reference to strArr

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,8 +1,6 @@
 const palindromes = function (str) {
-    return !str.split("")
-               .some(
-                    (item, index) => strArr[index] != strArr[-(index + 1)]
-                );
+    const strArr = str.split("");
+    return !strArr.some((item, index) => strArr[index] != strArr[-(index + 1)]);
 };
 
 // Do not edit below this line
