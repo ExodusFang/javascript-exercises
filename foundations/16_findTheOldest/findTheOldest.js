6e85941339ffd278aa9ef9449b7f20f11481a47c
ExodusFang <exodusfang@gmail.com>
2026-05-24 19:09:01 +1000

    Complete function stub

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(personArr) {
+    const calculateAge = person => person.yearOfDeath - person.yearOfBirth;
+    return personArr.reduce((prev, curr) => calculateAge(curr) > calculateAge(prev) ? curr : prev);
 };
 
 // Do not edit below this line
